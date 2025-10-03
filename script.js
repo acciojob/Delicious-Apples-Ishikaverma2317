28d312f9edd498082215d6aa92f80ed9e91fa495
Ishika Verma <ishikaverma2317@gmail.com>
2025-10-03 09:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+// Change main photo when album image is clicked
+const mainPhoto = document.querySelector('.photo-main img');
+const albumPhotos = document.querySelectorAll('.photo-album li img');
+
+albumPhotos.forEach(photo => {
+  photo.addEventListener('click', () => {
+    mainPhoto.src = photo.src;
+  });
+});
