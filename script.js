26fbb3d5223ae6bb246bdd6c374d3174e656417b
Ishika Verma <ishikaverma2317@gmail.com>
2025-10-03 09:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Change main photo when album image is clicked
-// const mainPhoto = document.querySelector('.photo-main img');
-// const albumPhotos = document.querySelectorAll('.photo-album li img');
+// // Change main photo when album image is clicked
+// // const mainPhoto = document.querySelector('.photo-main img');
+// // const albumPhotos = document.querySelectorAll('.photo-album li img');
 
-albumPhotos.forEach(photo => {
-  photo.addEventListener('click', () => {
-    mainPhoto.src = photo.src;
-  });
-});
+// albumPhotos.forEach(photo => {
+//   photo.addEventListener('click', () => {
+//     mainPhoto.src = photo.src;
+//   });
+// });
